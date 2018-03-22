4c9e032be0cac1a3a0ce155d3e12bebcbf67d0d1
Thomas Kioko <kiokotomas@gmail.com>
2018-03-22 23:23:30 +0300

    create app constants class
    - this class replaces `Config.java`

---
+++ app/src/main/java/com/myduka/app/util/AppConstants.java
@@ -0,0 +1,47 @@
+package com.myduka.app.util;
+
+/**
+ *
+ */
+
+public class AppConstants {
+
+    /**
+     * Connection timeout duration
+     */
+    public static final int CONNECT_TIMEOUT = 60 * 1000;
+    /**
+     * Connection Read timeout duration
+     */
+    public static final int READ_TIMEOUT = 60 * 1000;
+    /**
+     * Connection write timeout duration
+     */
+    public static final int WRITE_TIMEOUT = 60 * 1000;
+    /**
+     * Base URL
+     */
+    public static final String BASE_URL = "https://sandbox.safaricom.co.ke/";
+    /**
+     * global topic to receive app wide push notifications
+     */
+    public static final String TOPIC_GLOBAL = "global";
+
+    // broadcast receiver intent filters
+    public static final String REGISTRATION_COMPLETE = "registrationComplete";
+    public static final String PUSH_NOTIFICATION = "pushNotification";
+
+    // id to handle the notification in the notification tray
+    public static final int NOTIFICATION_ID = 100;
+    public static final int NOTIFICATION_ID_BIG_IMAGE = 101;
+    public static final String SHARED_PREF = "ah_firebase";
+
+    //STKPush Properties
+    public static final String BUSINESS_SHORT_CODE = "174379";
+    public static final String PASSKEY = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919";
+    public static final String TRANSACTION_TYPE = "CustomerPayBillOnline";
+    public static final String PARTYB = "174379";
+    public static final String CALLBACKURL = "https://spurquoteapp.ga/pusher/pusher.php?title=stk_push&message=result&push_type=individual&regId=";
+
+
+}
